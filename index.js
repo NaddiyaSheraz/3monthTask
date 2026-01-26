459584c76d5e4c00833491a3aaa4a8c8573be255
Naddiya Sheraz <naddiyasheraz18@gmail.com>
2026-01-26 11:03:00 +0500

    responsive navbar

---
+++ index.js
@@ -0,0 +1,8 @@
+
+    const toggle = document.getElementById("menuToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    toggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
