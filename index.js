52e769cd5f0d89cbf2cb7c8ec992c1235256dea8
NaddiyaSheraz <sherazn2004@gmail.com>
2026-05-09 13:43:50 +0500

    sec2 added

--- index.js
+++
@@ -1,8 +0,0 @@
-
-    const toggle = document.getElementById("menuToggle");
-    const navMenu = document.getElementById("navMenu");
-
-    toggle.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
-    });
-
